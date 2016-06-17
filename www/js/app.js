49a6f306bdb6707000f4f1d9b8547e1acad996ac
kingstakh <kingstakh@yandex.ru>
2016-06-17 10:09:14 +0300

    Update app.js

--- www/js/app.js
+++ www/js/app.js
@@ -16,8 +16,8 @@ angular.module('starter', ['ionic'])
     };
 
     // Update with your OneSignal AppId and googleProjectNumber before running.
-    window.plugins.OneSignal.init("b2f7f966-d8cc-11e4-bed1-df8f05be55ba",
-                                   {googleProjectNumber: "703322744261"},
+    window.plugins.OneSignal.init("a35c1178-2390-4c70-866c-78d93cffc4be",
+                                   {googleProjectNumber: "281905587763"},
                                    notificationOpenedCallback);
   });
 })
